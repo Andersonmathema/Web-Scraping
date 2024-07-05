25ff2af4a7e55bad2d65e2e638f4ddf3a750945b
Andersonmathema <ander.math89@gmail.com>
2024-07-05 00:37:55 -0300

    Atualização test

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 data  = 'C:\\Users\\anddr\\Downloads\\MAPAO_REYNALDODONASCIMENTOFALLEIROSDOUTOR-10236_2ªSERIEAMANHAANUAL_1.xlsx'
-planilha = pd.read_excel(data,skiprows=14 )
-#planilha = planilha.loc[:, ~planilha.columns.str.contains('^Unnamed')]
-planilha.dropna(axis=1, how='all', inplace=True)
-for plan in planilha.columns:
-    planilha.drop(planilha.columns[range(0,len(planilha),5)])
-print(planilha)
-# print(planilha[13:len(planilha)-1])
-#print(planilha.loc[13:len(planilha)-1].dropna)
-output_excel = 'C:\\Users\\anddr\\Downloads\\planilha_modificada.xlsx'
-planilha.to_excel(output_excel, index=False)
+planilha = pd.read_excel(data,skiprows=10 )
+
+def cabecalho():
+    lista_disciplinas = [planilha.loc[0].dropna()]    
+    planilha.columns = lista_disciplinas[:len(planilha.columns)] 
+
+
+if __name__ == '__main__':    
+    
+    planilha = pd.read_excel(data,skiprows=14 )
+    #cabecalho()
+    planilha.dropna(axis=1, how='all', inplace=True)
+    #planilha.drop()
+    print(planilha)
+
+# output_excel = 'C:\\Users\\anddr\\Downloads\\planilha_modificada.xlsx'
+# planilha.to_excel(output_excel, index=False)
